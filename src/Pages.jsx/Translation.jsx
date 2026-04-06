63895b3a9d96e84537871709a4b5cd935d075fe3
KyleWhitehead <kylewhitehead0033@yahoo.com>
2026-04-06 13:19:32 -0600

    Adjusted Files

--- src/Pages.jsx/Translation.jsx
+++ src/Pages.jsx/Translation.jsx
@@ -1,18 +1,11 @@
 import React from 'react'
 import './Translation.css'
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 //https://bible-api.com/data//
 
 const Translation = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/translations" element={<h1>Translations Page</h1>} />
-        <input type="text" placeholder='Search for a translation' className='search__input' />
-        <button className='search__button' >Search</button>
-      </Routes>
-    </Router>
+    <div>Translation</div>
   )
 }
 
