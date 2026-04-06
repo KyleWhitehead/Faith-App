d92d4d791f5dd0fcb42117e07a9609389b296c02
KyleWhitehead <kylewhitehead0033@yahoo.com>
2026-04-06 13:51:48 -0600

    Routing and Api call

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import Home from './Pages.jsx/Home';
 import About from './Pages.jsx/About';
 import Contact from './Pages.jsx/Contact';
 import Translation from './Pages.jsx/Translation';
+import Nav from './Pages.jsx/Nav';
+
 function App() {
   return (
       <Router>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/translation" target="_blank">Translations</Link>
-        </nav>
+        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
--- src/Pages.jsx/Translation.jsx
+++ src/Pages.jsx/Translation.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { use } from 'react'
 import './Translation.css'
+import { useParams } from 'react-router-dom'
+import { useEffect } from 'react';
 
 //https://bible-api.com/data//
 
 const Translation = () => {
+  const { translation } = useParams();
+  useEffect(() => {
+    fetch(`https://bible-api.com/data/${translation}`)
+      .then(response => response.json())
+  }, []);
+
+
   return (
-    <div>Translation</div>
+    <h1>Translation</h1>
   )
 }
 
